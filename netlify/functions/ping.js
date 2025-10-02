f8f7b4acd82a241c2ee41a825846ddc238845e93
natural-uncle888 <jesn.lin@gmail.com>
2025-10-03 01:42:12 +0800

    Delete netlify/functions/ping.js

--- netlify/functions/ping.js
+++
@@ -1,5 +0,0 @@
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
-  body: JSON.stringify({ ok: true, now: new Date().toISOString() })
-});
